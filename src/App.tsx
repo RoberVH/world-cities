f7398a7398374e20265e598fc78b62ab0393889e
RoberVH <roberto.vicuna@gmail.com>
2020-03-17 17:02:23 -0600

    1er Probando API OpenWeatherMap

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import ciudades from './data/city.list.json';
+
+
+// 
+
+//declare function buscar_ciudadesxpais(ciudades:String[]):String[];
+
+interface Ciudad  {
+     id: number;
+    name: string;
+    state: string;
+    country: string;
+    coord: {
+      lon: number;
+      lat: number
+    }  
+}
+
+const [reporteClima,setReporte]=useState({});
+
+
+// const asigna_ciudades=(lista:Ciudad[]):Ciudad[] => {
+//   return lista;
+// }
+
+const APIURL= 'api.openweathermap.org/data/2.5/weather';
+const APIKEY= process.env.OPENWEATHERMAP_APIKEY
+
+const getAXIOSCiudades = (param: string) => axios.get(`${APIURL}/?1q=${param}&units=metric&lang=es&apiid=${APIKEY}`);
+
+// // const get_ciudades: (ciudades:Ciudad[]) => Ciudad[] =
+// //     (lciudades: Ciudad[]) => { return  lciudades.filter((city:Ciudad) => (city.country)==='MX'); }
+
+// // let ciudades_Mexico=get_ciudades(lista);
+
+
+const buscarCiudades = async (e: any) => {
+// 
+const listaCiudades = await getAXIOSCiudades('Saltillo');
+setReporte(listaCiudades);
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+         <button onClick={buscarCiudades}>Buscar Ciudades</button>
+      </div>
+      <hr></hr>
+      <div>
+        {reporteClima}
+      </div>
+  
     </div>
   );
 }
